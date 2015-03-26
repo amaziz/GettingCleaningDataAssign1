ba2c7568483d3b5b088084c39ad273a0809d221d
amaziz <contact@amaziz.com>
2015-03-26 08:58:51 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -17,18 +17,20 @@ ytest <- read.csv("test/y_test.txt", header = FALSE, sep="",  col.names="y") # r
 subjecttest <-read.csv("test/subject_test.txt", header = FALSE, sep="", col.names="subjectid") # read the subject ids for the training set
 test<- cbind(test, ytest, subjecttest) # attach the y  and subjectid variables
 
-# Merge data (training and test sets) with their labels
+# bind data (training and test sets)
 alldata<- rbind(train, test)
+
+# data is merged with labels
 mergeddata <- merge(feature.labels,alldata, by.y="y", by.x="V1")
 
 # rename the V2 column to activity
 mergeddata<- rename(mergeddata, activity = V2)
 
-# remove the y column (activity id)
+# remove the id column after merge
 mergeddata<- select(mergeddata, -V1)
 
 # Group by Subject Id and 
 groupset <- group_by(mergeddata, subjectid, activity)
-tidyset <- summarise_each(newDataset, funs(mean))
+tidyset <- summarise_each(groupset, funs(mean))
 
 write.table(tidyset, file="tidyset.txt", row.names=FALSE)
